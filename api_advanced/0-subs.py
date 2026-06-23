ca60d5147ada22b89af4be1f922837e03de0f1e3
kevinishimwe2 <k.ishimwe2@alustudent.com>
2026-06-23 14:41:03 +0200

    new 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,14 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and retrieve the subscriber count
-for a given subreddit.
-"""
+"""Module to query Reddit API for subscriber count."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    #
-    # Queries the Reddit API and returns the total number of subscribers
-    # for a given subreddit. If the subreddit is invalid, returns 0.
-    # 
-    if not subreddit or not isinstance(subreddit, str):
-        return 0
-
+    """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:api_advanced:v1.0.0 (by /u/custom_user)"
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-    except Exception:
+    headers = {"User-Agent": "Mozilla/5.0 (custom) api_advanced/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-
-    return 0
+    data = response.json()
+    return data.get("data", {}).get("subscribers", 0)
