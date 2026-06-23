8e02d21a396fe5131acf4cf1b48cd6c8904837ea
kevinishimwe2 <k.ishimwe2@alustudent.com>
2026-06-23 14:33:41 +0200

    2

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API and print the titles of the first
+10 hot posts for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    
+    #  /**Prints the titles of the first 10 hot posts listed for a given subreddit,
+    # If the subreddit is invalid, prints None.**/
+    
+    if not subreddit or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:api_advanced:v1.0.0 (by /u/custom_user)"
+    }
+    params = {"limit": 10}
+
+    try:
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            children = data.get("data", {}).get("children", [])
+            for child in children:
+                print(child.get("data", {}).get("title"))
+        else:
+            print("None")
+    except Exception:
+        print("None")
