6df2a651cba72bb711b1fa53e86acf9ec984952f
kevinishimwe2 <k.ishimwe2@alustudent.com>
2026-06-23 14:41:52 +0200

    new 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and print the titles of the first
-10 hot posts for a given subreddit.
-"""
+"""Module to query Reddit API for top 10 hot posts."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is invalid, prints None.
-
-    """
-    if not subreddit or not isinstance(subreddit, str):
-        print("None")
-        return
-
+    """Print the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:api_advanced:v1.0.0 (by /u/custom_user)"
-    }
-    # Limit to 10 posts to retrieve only the necessary amount of data
+    headers = {"User-Agent": "Mozilla/5.0 (custom) api_advanced/1.0"}
     params = {"limit": 10}
-
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            children = data.get("data", {}).get("children", [])
-            for child in children[:10]:
-                print(child.get("data", {}).get("title"))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    for post in posts:
+        print(post.get("data", {}).get("title"))
